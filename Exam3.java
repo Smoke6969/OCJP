4d45d2b76e7e6713016b967143c3aea5894dddb9
Oleksandr Smolka <o.smolka@astoundcommerce.com>
2018-11-29 09:39:28 +0200

    continue

--- Exam3.java
+++ Exam3.java
@@ -9,7 +9,7 @@ import java.util.ListIterator;
 public class Exam3 {
 
 	// new ArrayList() and new ArrayList<>() is basically the same constructors. It is done in java for compatibility
-	// with versions before Generics. Waht happens is Type Errasure:
+	// with versions before Generics. What happens is Type Errasure:
 	// When people mention "Type Erasure" this is what they're talking about. The compiler inserts the casts for you,
 	// and then 'erases' the fact that it's meant to be a list of Person not just Object
 	public void question1(){
