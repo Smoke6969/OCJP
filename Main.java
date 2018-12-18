e2a81388b1d420d08c840b1ad88cfff2c72913fc
Oleksandr Smolka <o.smolka@astoundcommerce.com>
2018-12-18 13:59:01 +0200

    continue

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 package myexamcloud;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException {
 
 		myexamcloud.Exam3 exam = new myexamcloud.Exam3();
 
-		exam.question17();
+		exam.question24();
 	}
 }
